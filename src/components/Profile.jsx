8910afb85213282fa8ed63330ff5c0cf6de13a7f
crashberry <crashberry@outlook.com>
2024-05-17 15:21:13 +0300

    Fix Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,21 +4,23 @@ export const Profile = () => {
       <div className="col-md-4">
         <img src="" alt="" />
       </div>
-      <p>
-        <span>Id:</span>
-        <span>2</span>
-      </p>
-      <p>
-        <span>Email:</span>
-        <span>vania@mail.ru</span>
-      </p>
-      <p>
-        <span>Фамилия и имя:</span>
-        <span>Иван Иванов</span>
-      </p>
-      <p>
-        Рассказ о себе
-      </p>
+      <div className="col-md-8">
+        <p>
+          <span>Id:</span>
+          <span>2</span>
+        </p>
+        <p>
+          <span>Email:</span>
+          <span>vania@mail.ru</span>
+        </p>
+        <p>
+          <span>Фамилия и имя:</span>
+          <span>Иван Иванов</span>
+        </p>
+        <p>
+          Рассказ о себе
+        </p>
+      </div>
     </div>
   );
 };
