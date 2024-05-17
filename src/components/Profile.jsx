36fac94ca42b4dc91aaa3e926fe9e857e31eb942
crashberry <crashberry@outlook.com>
2024-05-17 15:18:53 +0300

    new commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,3 +1,24 @@
 export const Profile = () => {
-  return <h1>Профиль</h1>;
+  return (
+    <div className="row">
+      <div className="col-md-4">
+        <img src="" alt="" />
+      </div>
+      <p>
+        <span>Id:</span>
+        <span>2</span>
+      </p>
+      <p>
+        <span>Email:</span>
+        <span>vania@mail.ru</span>
+      </p>
+      <p>
+        <span>Фамилия и имя:</span>
+        <span>Иван Иванов</span>
+      </p>
+      <p>
+        Рассказ о себе
+      </p>
+    </div>
+  );
 };
